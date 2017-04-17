c9b25ebc477106e5fee034525f54218e1ae613e2
darci <darcikhey27@gmail.com>
2017-04-16 20:44:02 -0700

    fixed logic on process.c

--- lab5/Lab5/process/process.c
+++ lab5/Lab5/process/process.c
@@ -4,10 +4,11 @@
 #include "process.h"
 
 void forkIt(char ** argv) {
-    // execute the commands in teh 2d array argv
     
     pid_t pid = fork();
+   
     int status = 0;
+   
     if(pid != 0) {
         waitpid(pid, &status, 0);
     }
