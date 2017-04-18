8f3b8cbecad758591f61cfda7552788dcf15eca8
darci <darcikhey27@gmail.com>
2017-04-17 22:04:57 -0700

    working on pipe command

--- lab6/Lab6/pipes/pipes.c
+++ lab6/Lab6/pipes/pipes.c
@@ -32,9 +32,33 @@ char** parsePrePipe(char *s, int* preCount) {
     char *token;
     char line[MAX];
     strcpy(line, s);
-    token = strtok(s, "|");
-    printf("token is %s\n", token);
+    token = strtok(line, "|");
+    strip(token);
+    //printf("token is %s\n", token);
     
+    char **argv = NULL;
+    int argc = 0;
+    
+    argc = makeArgs(token, &argv);
+
+    printargs(argc, argv);
+    *preCount = argc;
+    
+    return argv;
+}
+
+char** parsePostPipe(char *s, int *postCount) {
+   
+    //printf("s = %s\n", s);
+
+    char *token;
+    char line[MAX];
+    strcpy(line, s);
+    token = strtok(line, "|");
+    token = strtok(NULL, "|");
+    strip(token);
+    //printf("token is %s\n", token);
+
     char **argv = NULL;
     int argc = 0;
     
@@ -45,23 +69,34 @@ char** parsePrePipe(char *s, int* preCount) {
     return argv;
 }
 
-char** parsePostPipe(char *s, int *postCount) {
-    puts("in postcount"); 
-    printf("s is %s\n", s);
-    char **argv = NULL;
-    int argc = 0;
-    char right[MAX];
-    strcpy(right, "darci");
-
-    argc = makeArgs(right, &argv);
-    printargs(argc, argv);
-    *postCount = argc;
-    printf("postcount %d\n", *postCount);
-    return argv;
-}
-
 void pipeIt(char ** prePipe, char ** postPipe) {
 
     // take the two commands and work it
-    puts("about to pipe");
+	pid_t pid;
+	int fd[2], res, status;
+	res = pipe(fd);
+
+	if(res < 0) {
+		printf("Pipe Failure\n");
+		exit(-1);
+	}
+	pid = fork();
+
+	if(pid != 0) {
+        close(fd[1]);
+        close(0);
+        dup(fd[0]);
+        close(fd[0]);
+
+        //execlp("wc", "wc", "-w", NULL);
+        execvp(postPipe[0], postPipe);
+	} 
+    else {
+		close(fd[0]);
+		close(1);
+		dup(fd[1]);
+		close(fd[1]);
+		//execlp("ls", "ls", "-l", NULL);
+        execvp(prePipe[0], prePipe);
+	}
 }
