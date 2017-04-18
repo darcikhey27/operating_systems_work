9cea017a32291aef4919f9b4f1e27097896a1118
darci <darcikhey27@gmail.com>
2017-04-17 18:42:41 -0700

    first part of laba done

---
+++ lab6/Lab6/pipes/pipes.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+
+#include "pipes.h"
+
+int containsPipe(char *s) {
+    
+    
+    
+    return 0;
+}
+
+char** parsePrePipe(char *s, int* preCount) {
+    
+    return NULL;
+}
+
+char** parsePostPipe(char *s, int *postCount) {
+
+    return NULL;
+}
---
+++ lab6/Lab6/process/process.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include "process.h"
+
+void forkIt(char ** argv) {
+    
+    pid_t pid = fork();
+   
+    int status = 0;
+   
+    if(pid != 0) {
+        waitpid(pid, &status, 0);
+    }
+    else {
+        execvp(argv[0], argv);
+        exit(-1);
+    }
+}
