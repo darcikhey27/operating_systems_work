f30c1a08c9ab84ecd1ec2817e301c19805e41f2f
darci <darcikhey27@gmail.com>
2017-04-22 00:47:35 -0700

    struguling with linker error

--- TheShell/alias/aliasUtil.h
+++ TheShell/alias/aliasUtil.h
@@ -1,12 +1,6 @@
 #ifndef ALIASUTIL_H_
 #define ALIASUTIL_H_
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "../linkedlist/linkedList.h"
-#include "../utils/myUtils.h"
-#include "../tokenize/makeArgs.h"
 
 /* will traverse the alias linkedlist, if it
  * finds a match of s it will execute the 
